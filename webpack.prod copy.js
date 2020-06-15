f5834129bce22bb617f027982fdc7e33326ae627
cmorales <crisdavidmm95.dev@gmail.com>
2020-06-15 14:37:20 -0500

    DOCS creados

--- webpack.prod copy.js
+++ webpack.prod copy.js
@@ -9,7 +9,7 @@ module.exports = {
     optimization: {
         minimizer: [ new OptimizeCssAssetsPlugin() ]
     },
-    output: { // Hash para evitar cookies
+    output: {
         filename: 'main.[contentHash].js'
     },
     module: {
@@ -73,3 +73,4 @@ module.exports = {
     ]
 
 }
+
